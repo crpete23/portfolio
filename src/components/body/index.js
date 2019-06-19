5f64e728adc57334cfb2dfd778aa161ef557cb3e
cpeterson <cpeterson@collineargroup.com>
2019-06-19 10:03:02 -0700

    initial layout complete

---
+++ src/components/body/index.js
@@ -0,0 +1,13 @@
+import About from './About'
+import Contact from './Contact'
+import Portfolio from './Portfolio'
+import Resume from './Resume'
+import Testimonials from './Testimonials'
+
+export {
+    About,
+    Contact,
+    Portfolio,
+    Resume,
+    Testimonials
+}
